c9bba14f707bb949bbd7f6adfb43d800a77d6aa8
TBWright <tbwrightartist@gmail.com>
2020-05-19 18:22:40 -0500

    FKChain RigUnit addition
    The FKChain node is a all-in-one node to create an FK chain of arbitrary length.

---
+++ Source/ControlRigExt/Private/Units/RigUnit_FKChain.cpp
@@ -0,0 +1,96 @@
+﻿
+
+#include "RigUnit_FKChain.h"
+#include "Units/RigUnitContext.h"
+
+FRigUnit_FKChain_Execute()
+{
+    DECLARE_SCOPE_HIERARCHICAL_COUNTER_RIGUNIT()
+    FRigBoneHierarchy* Hierarchy = ExecuteContext.GetBones();
+    if (Hierarchy)
+    {
+        switch (Context.State)
+        {
+        case EControlRigState::Init:
+            {                
+                for (int i = 0; i < JointControllerPairs.Num(  ); i++)
+                {
+                    FRigUnit_FKChain_JointControllerPair& Pair = JointControllerPairs[i];
+                    
+                    Pair.CachedBoneIndex = Hierarchy->GetIndex( Pair.Bone );
+                    
+                    Hierarchy->GetChildren( Pair.CachedBoneIndex, Pair.ValidChildren, true );
+                    if (i > 0)
+                    {
+                        if (!JointControllerPairs[i-1].ValidChildren.Contains( Pair.CachedBoneIndex ))
+                        {
+                            UE_CONTROLRIG_RIGUNIT_REPORT_WARNING( TEXT("All FK Chain Bones must be in a single chain."));
+                            return;
+                        }
+                    }
+                }
+                return;
+            }
+        case EControlRigState::Update:
+            {
+                if (Weight <= SMALL_NUMBER || JointControllerPairs.Num(  ) < 1 )
+                {
+                    return;
+                }
+                for (FRigUnit_FKChain_JointControllerPair& Pair : JointControllerPairs)
+                {
+                    if (Pair.CachedBoneIndex != INDEX_NONE)
+                    {
+                        switch (Pair.Space)
+                        {
+                            case EBoneGetterSetterMode::GlobalSpace:
+                                {
+                                    if (FMath::IsNearlyEqual( Weight, 1.f ))
+                                    {
+                                        Hierarchy->SetGlobalTransform( Pair.Bone,
+                                                                       Pair.Transform,
+                                                                       Pair.bPropagateToChildren );
+                                    }
+                                    else
+                                    {
+                                        float T = FMath::Clamp<float>(Weight, 0.f, 1.f);
+                                        const FTransform PreviousTransform = Hierarchy->GetGlobalTransform( Pair.Bone );
+                                        Hierarchy->SetGlobalTransform( Pair.Bone,
+                                                                       FControlRigMathLibrary::LerpTransform( PreviousTransform, Pair.Transform, T ),
+                                                                       Pair.bPropagateToChildren);
+                                    }
+                                    break;
+                                }
+                            case EBoneGetterSetterMode::LocalSpace:
+                                {
+                                    if (FMath::IsNearlyEqual(Weight, 1.f))
+                                    {
+                                        Hierarchy->SetLocalTransform(Pair.Bone,
+                                                                     Pair.Transform,
+                                                                     Pair.bPropagateToChildren);
+                                    }
+                                    else
+                                    {
+                                        float T = FMath::Clamp<float>(Weight, 0.f, 1.f);
+                                        const FTransform PreviousTransform = Hierarchy->GetLocalTransform(Pair.Bone);
+                                        Hierarchy->SetLocalTransform(Pair.Bone,
+                                                                     FControlRigMathLibrary::LerpTransform(PreviousTransform, Pair.Transform, T),
+                                                                     Pair.bPropagateToChildren);
+                                    }
+                                    break;
+                                }
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                    }
+                }
+            }
+        default:
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Source/ControlRigExt/Private/Units/RigUnit_FKChain.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include "ControlRig/Private/Units/Highlevel/RigUnit_HighlevelBase.h"
+#include "RigUnit_FKChain.generated.h"
+
+USTRUCT()
+struct FRigUnit_FKChain_JointControllerPair
+{
+    GENERATED_BODY( )
+
+    /**
+    * The name of the Bone to set the transform for.
+    */
+    UPROPERTY(meta = (Input, Constant, CustomWidget = "BoneName"))
+    FName Bone;
+
+    /**
+    * The transform value to set for the given Bone.
+    */
+    UPROPERTY(meta = (Input, Output))
+    FTransform Transform;
+
+    /**
+    * Defines if the bone's transform should be set
+    * in local or global space.
+    */
+    UPROPERTY(meta = (Input))
+    EBoneGetterSetterMode Space;
+
+    /**
+    * If set to true all of the global transforms of the children
+    * of this bone will be recalculated based on their local transforms.
+    * Note: This is computationally more expensive than turning it off.
+    */
+    UPROPERTY(meta = (Input, Constant))
+    bool bPropagateToChildren = true;
+
+    int32 CachedBoneIndex;
+    TArray<int32> ValidChildren;
+};
+
+/**
+* Applies a simple FK chain to a joint chain without the needs for extra nodes
+*/
+USTRUCT(meta=(DisplayName="FK Chain", Category="Controls Ext", Keywords="FK,Chain"))
+struct FRigUnit_FKChain : public FRigUnit_HighlevelBaseMutable
+{
+    GENERATED_BODY()
+
+    RIGVM_METHOD()
+    virtual void Execute(const FRigUnitContext& Context) override;
+
+    FRigUnit_FKChain()
+    {
+        Weight = 1.f;
+    }
+
+    /**
+     * The joint/controller pairs that should be coupled. Each successive joint MUST
+     * be lower in the hierarchy than the previous.
+     */
+    UPROPERTY(meta=(Input))
+    TArray<FRigUnit_FKChain_JointControllerPair> JointControllerPairs;
+
+    /** 
+    * The weight of the chain - how much of the FK should be applied
+    */
+    UPROPERTY(meta = (Input, UIMin = "0.0", UIMax = "1.0"))
+    float Weight;
+    
+};
